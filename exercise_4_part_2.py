5c08e1574e84f5b8cb29ede66e54c1ce6ad807e0
Guido Marinelli <72472738+GuidoMarinelli@users.noreply.github.com>
2023-07-14 00:04:46 +0200

    Add files via upload

--- exercise_4_part_2.py
+++ exercise_4_part_2.py
@@ -8,40 +8,41 @@
 #
 # Description: Lezione 7 - Command line: Soluzione esercizio 4 parte 2.
 #
+
 import argparse
 
 
-def new_element():
-    """
-    Funzione che chiede in modo interattivo all’utente le informazioni necessarie ad aggiungere un nuovo elemento
-    alla rubrica.
-    """
-    name = input('Inserisci il nome e cognome del nuovo contatto: ')
-    day = input('Inserisci il giorno di nascita del nuovo contatto: ')
-    month = input('Inserisci il mese di nascita del nuovo contatto: ')
-    year = input("Inserisci l'anno di nascita del nuovo contatto: ")
-    age = input("Inserisci l'età del nuovo contatto: ")
-    sex = input('Inserisci il sesso del nuovo contatto: ')
-    mail = input("Inserisci l'indirizzo email del nuovo contatto: ")
-    return f'{name}, {day}, {month}, {year}, {age}, {sex}, {mail}\n'
-
-
-def update_rubrica(file_txt, new_contact):
+def update_rubrica(filetxt, new_contact):
     """Funzione che aggiorna un file 'rubrica' passatole."""
-    with open(file_txt, 'r+') as textwritefile:
-        data = textwritefile.read().strip('\n')
-        textwritefile.seek(0, 0)
-        textwritefile.write(data + '\n')
+    with open(filetxt, 'a') as textwritefile:
         textwritefile.write(new_contact)
 
 
 parser = argparse.ArgumentParser(prog="exercise_4_part_2.py",
                                  description="Chiede e aggiunge un nuovo elemento nel file rubrica.txt",
                                  epilog="G.Marinelli, 2023/06/07")
-parser.add_argument('-n', '--file_name', default=None, help='Nome del file da cui leggere/scrivere la rubrica')
+
+parser.add_argument('-f', '--filename', default='rubrica.txt', help='Nome del file da cui leggere/scrivere la rubrica')
+parser.add_argument('-n', '--name', default=None, help='Nome del contatto da inserire nella rubrica')
+parser.add_argument('-s', '--surname', default=None, help='Cognome del contatto da inserire nella rubrica')
+parser.add_argument('-d', '--day', default=None, help='Giorno di nascita del contato')
+parser.add_argument('-m', '--month', default=None, help='Mese di nascita del contatto')
+parser.add_argument('-y', '--year', default=None, help='Anno di nascita del contatto')
+parser.add_argument('-a', '--age', default=None, help='Età del contatto')
+parser.add_argument('-g', '--gender', default=None, help='Sesso/genere del conatto')
+parser.add_argument('-e', '--email', default=None, help='Indirizzo e-mail del contatto')
 
 args = parser.parse_args()
-file_rubrica = args.file_name
 
-contact = new_element()
-update_rubrica(file_rubrica, contact)
+filename = args.filename
+name = args.name
+surname = args.surname
+day = args.day
+month = args.month
+year = args.year
+age = args.age
+gender = args.gender
+email = args.email
+
+contact = f'{name} {surname}, {day}, {month}, {year}, {age}, {gender}, {email}\n'
+update_rubrica(filename, contact)
